34594d67914b306567802c6370dc31a8aa8dbde6
willch <williamchenthu@gmail.com>
2017-01-08 20:15:07 -0800

    test: add the jar and revise the junit class
    body: the details of how to run junit, please see SetUpForGunit file.

--- proj1/ArrayDequeTest.java
+++ proj1/ArrayDequeTest.java
@@ -1,5 +1,24 @@
 /**
  * Created by juanyan.li on 1/8/17.
  */
-public class ArrayDequeTest {
+import static org.junit.Assert.assertEquals;
+import junit.framework.*;
+
+public class ArrayDequeTest extends TestCase {
+	protected ArrayDeque<Integer> deque;
+
+	// Methods run before each unit test case.
+	protected void setUp() {
+		this.deque = new ArrayDeque<Integer>();
+	}
+
+	// method can be run after each unit test is
+	// protected void tearDown() {}
+
+	// Unit test for init queue has zero length.
+	// Notice all the unit test should
+	// start will test then + other words.
+	public void testInitSizeZero() {
+		assertEquals(deque.size(), 0);
+	}
 }
